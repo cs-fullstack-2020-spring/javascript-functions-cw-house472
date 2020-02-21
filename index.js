b489619f38dda7dc320a4abcfd72ebd24237280a
house472 <morehouse472@hotmail.com>
2020-02-21 14:45:04 -0600

    functions classwork

---
+++ index.js
@@ -0,0 +1,65 @@
+// ### Problem 1
+
+// Create a ```printNumbers``` function to print integers from -25 to 20.
+// create function and print
+// let countNumbers;
+// function printNumbers (num1){
+//  console.log(num1)
+// }
+// for(let i=-20;i<=20;i++){
+//     console.log(i)
+// }
+
+
+
+
+// ### Problem 2
+// Create a function that’s passed a firstName and lastName. Return lastName comma firstName.
+// assign variable for first and last name
+// let firstName="charles"
+// let lastName= "gordon"
+// // create names function
+// function names(firstName,lastName){
+//     console.log(lastName,firstName)
+// }
+// // call function
+// names(firstName,lastName)
+
+
+// ### Problem 3
+// Create a function in your program that counts from 0 to [NUMBER]
+// let number;
+// function count(num1){
+//     console.log(count)
+// }
+// for(i=0; i= number;i=i++){
+//     number=10
+//     console.log(i)
+// }
+// count(number)
+
+// ### Problem 4
+// Create a function called checkPassword. Send two string variables to the checkPassword function to check if the strings are equal. Return true if they are equal and false if they are not equal. Print the function's return value.
+let word1="word"
+let word2="word2"
+function checkPassword(variable1,variable2){
+    console.log(variable1,variable2)
+}
+if(word1!=word2){
+    console.log(true)
+} else{
+    console.log(false)
+}
+word2="word"
+// ### Problem 5
+// Create a ```sumOf3Numbers``` function that will print out the sum of the three numbers using the starting code below:
+// ```javascript
+// var number1 = 0;
+// var number2 = 0;
+// var number3 = 0;
+
+// sumOf3Numbers(number1, number2, number3);
+// ```
+
+// ### Challenge
+// Create 4 functions called add, subtract, multiply, and divide. Create them to allow a user to perform the name of the function to the two numbers and return the result.
